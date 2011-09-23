bf383d261a657e5c2e1c5e7ff950f6f4b1858475
Merlin <merlinvn@gmail.com>
2011-09-23 18:32:13 +0700

    Finished project

--- lib/book_rename.rb
+++ lib/book_rename.rb
@@ -1,5 +1,49 @@
 require "book_rename/version"
 
 module BookRename
-  # Your code goes here...
+  require "amazon_product"
+
+
+  def BookRename.req
+    @req ||= AmazonProduct["us"]
+    @req.configure do |c|
+      c.key    = "05Z3HTZN2GSB5ZYRXC02"
+      c.secret = "DfS64S9trXsg8J0SQvvZKxwV+YSjzfao/4CdDDSV"
+      c.tag    = "nosters-20"
+    end
+    @req
+  end
+
+
+  def BookRename.find_book_by_isbn_10 (isbn)    
+    # resp = req.search('Books',:response_group => %w{ItemAttributes Images},
+    #     :power => isbn)
+
+    resp = req.find(isbn)
+    mybook = Hash.new
+    mybook['ISBN'] = isbn
+    mybook['Title'] =  'null'
+    mybook['Publisher'] = 'null'
+    
+    resp['Item'].each do |item|
+      mybook['ISBN'] = item['ASIN']
+      mybook['Title'] =  item['ItemAttributes']['Title'].gsub(/#/,'Sharp').gsub(/C\+\+/,'Cpp').gsub(/[^\(\)\-\s\.\w]/,'')
+      mybook['Publisher'] =  item['ItemAttributes']['Manufacturer'].gsub(/[\'\s]/,'.')
+    end
+    mybook
+  end
+  
+  def BookRename.get_file_name(mybook)
+    [mybook['Publisher'], mybook['Title'], mybook['ISBN'], 'pdf'].join('.')
+  end  
+  
+  def BookRename.extract_isbn(filename)
+    result = ""
+    if filename =~ /(^|\D+)(\d{10}|\d{9}X)\D*/
+        result = $2
+    end
+    result
+  end
+  
 end
+
--- lib/book_rename/cli.rb
+++ lib/book_rename/cli.rb
@@ -1,10 +1,53 @@
 require 'thor'
+require 'book_rename'
+# require "FileUtils"
 
 module BookRename
   class CLI < Thor    
-    desc "Say message", "Say somthing in concolse"
-    def say(message)
-      puts message
+    default_task :search
+    # extend BookRename
+
+    desc "search isbn", "Search book by isbn"
+    def search(isbn)
+      mybook =BookRename.find_book_by_isbn_10 isbn
+      puts BookRename.get_file_name(mybook)
+      mybook
     end    
+
+    desc "extract_isbn filename", "Extract isbn from file name"
+    def extract_isbn filename
+      puts BookRename.extract_isbn filename
+    end
+
+    desc "list", ""
+    def list()
+      Dir.glob("*.*") { |filename| 
+        puts filename        
+      }
+    end
+
+    desc "rename path", "rename all pdf file in the path"
+    def rename path='.'
+      done_folder = 'done'
+      Dir.chdir(path)
+      if not File.directory?( done_folder)
+        puts "creating directory \"#{done_folder}\""
+        Dir.mkdir("done")
+      end
+      Dir.glob("*.pdf") { |filename| 
+        isbn = BookRename.extract_isbn filename
+        if isbn.length > 0
+          mybook = BookRename.find_book_by_isbn_10 isbn
+          newFileName = BookRename.get_file_name(mybook)
+          puts newFileName
+          newFileName = (done_folder + "/" + newFileName)
+          File.rename(filename, newFileName)
+        end
+      }
+      
+      
+
+    end
+
   end
 end
